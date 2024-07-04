8553f27c9f1537d90abca46a32998888912b12b8
roopesh.sagar <roopesh.sagar@beehyv.com>
2024-07-04 19:48:37 +0530

    Reapply "AddParty Changes and integration with InsideHearin"
    This reverts commit fce0e8e151d6fb252d4fa3eacb7e5bcf309c1372.

--- frontend/micro-ui/web/micro-ui-internals/packages/modules/hearings/src/pages/employee/InsideHearingMainPage.js
+++ frontend/micro-ui/web/micro-ui-internals/packages/modules/hearings/src/pages/employee/InsideHearingMainPage.js
@@ -6,6 +6,8 @@ import EvidenceHearingHeader from "./EvidenceHeader";
 import HearingSideCard from "./HearingSideCard";
 import MarkAttendance from "./MarkAttendance";
 import debounce from "lodash/debounce";
+import AddParty from "./AddParty";
+import { add } from "lodash";
 
 const fieldStyle = { marginRight: 0 };
 
@@ -20,11 +22,21 @@ const InsideHearingMainPage = () => {
   const [options, setOptions] = useState([]);
   const [additionalDetails, setAdditionalDetails] = useState({});
   const [selectedWitness, setSelectedWitness] = useState({});
+  const [addPartyModal, setAddPartyModal]= useState(false);
   const textAreaRef = useRef(null);
   const [isOpen, setIsOpen] = useState(false);
   const [attendees, setAttendees] = useState([]);
   const tenantId = window?.Digit.ULBService.getCurrentTenantId();
-  const { hearingId: hearingId } = Digit.Hooks.useQueryParams(); // query paramas
+  // const { hearingId: hearingId } = Digit.Hooks.useQueryParams(); // query paramas
+  const hearingId ="HEARING-ID-2024-06-12-000029";
+
+  const onCancel= ()=>{
+    setAddPartyModal(false);
+  }
+
+  const onClickAddWitness = ()=>{
+    setAddPartyModal(true);
+  }
 
   if (!hearingId) {
     const contextPath = window?.contextPath || "";
@@ -65,10 +77,10 @@ const InsideHearingMainPage = () => {
         const additionalDetails = hearingData?.additionalDetails || {};
         const processedAdditionalDetails = {
           ...additionalDetails,
-          witnesss: additionalDetails.witnesss || [],
+          witnesses: additionalDetails.witnesses || [],
         };
         setAdditionalDetails(processedAdditionalDetails);
-        setOptions(processedAdditionalDetails.witnesss.map((witness) => ({ label: witness.name, value: witness.name })));
+        setOptions(processedAdditionalDetails.witnesses.map((witness) => ({ label: witness.name, value: witness.name })));
         setImmediateText(hearingData?.transcript[0]);
         setDelayedText(hearingData?.transcript[0]);
         setSelectedWitness(processedAdditionalDetails.witnesses[0] || {});
@@ -92,9 +104,9 @@ const InsideHearingMainPage = () => {
 
         const updatedHearing = { ...prevHearing };
         if (activeTab === "Witness Deposition") {
-          const witnessIndex = updatedHearing.additionalDetails.witnesss.findIndex((w) => w.name === selectedWitness);
+          const witnessIndex = updatedHearing.additionalDetails.witnesses.findIndex((w) => w.name === selectedWitness);
           if (witnessIndex >= 0) {
-            updatedHearing.additionalDetails.witnesss[witnessIndex].deposition = newText;
+            updatedHearing.additionalDetails.witnesses[witnessIndex].deposition = newText;
           }
         } else {
           updatedHearing.transcript[0] = newText;
@@ -122,7 +134,7 @@ const InsideHearingMainPage = () => {
 
   const handleDropdownChange = (event) => {
     const selectedName = event.target.value;
-    const selectedWitness = additionalDetails.witnesss.find((w) => w.name === selectedName);
+    const selectedWitness = additionalDetails.witnesses.find((w) => w.name === selectedName);
     setSelectedWitness(selectedWitness);
     setWitnessDepositionText(selectedWitness?.deposition || "");
   };
@@ -160,7 +172,7 @@ const InsideHearingMainPage = () => {
                   color: "#007E7E",
                   fontWeight: 700,
                 }}
-                // onClick={() => console.log("click")}  // for add new witness
+                onClick={onClickAddWitness}
               >
                 + Add New Witness
               </button>
@@ -288,6 +300,16 @@ const InsideHearingMainPage = () => {
           )}
         </div>
       </ActionBar>
+      <div>
+        {addPartyModal && <AddParty 
+        onCancel={onCancel} 
+        onDismiss={onCancel} 
+        hearing= {hearing} 
+        tenantId={tenantId}
+        hearingId={hearingId}
+        setHearing={setHearing}
+        ></AddParty>}
+      </div>
     </div>
   );
 };
